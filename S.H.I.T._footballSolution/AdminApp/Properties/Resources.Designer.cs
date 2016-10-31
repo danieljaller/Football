d7c4ce876585167e2da69aef23b5faf167efe651
Martin Lindstedt <lindstedt.martin@outlook.com>
2016-10-31 14:28:11 +0100

    Removed and added AdminApp.

--- S.H.I.T._footballSolution/AdminApp/Properties/Resources.Designer.cs
+++ S.H.I.T._footballSolution/AdminApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace S.H.I.T._footballSolution.Properties
+namespace AdminApp.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace S.H.I.T._footballSolution.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("S.H.I.T._footballSolution.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AdminApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
