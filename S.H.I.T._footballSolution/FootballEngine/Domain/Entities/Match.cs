82dbe93f4c818b6ab327b7ab20c0881d84b0206e
Daniel Jaller <danieljaller@gmail.com>
2016-10-31 14:48:39 +0100

    Deleted empty class "Class1" and added class "Match" in FootballEnginge/Domain/Entities

--- S.H.I.T._footballSolution/FootballEngine/Domain/Entities/Match.cs
+++ S.H.I.T._footballSolution/FootballEngine/Domain/Entities/Match.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FootballEngine
+namespace FootballEngine.Domain.Entities
 {
-    public class Class1
+    class Match
     {
     }
 }
