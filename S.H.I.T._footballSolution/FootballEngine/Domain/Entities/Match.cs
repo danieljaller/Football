6c3de25488a3e80067bfbf60ca91ea51732eea7a
Linus Gustafsson <osnusmeforfasensatan@hotmail.com>
2016-11-02 10:36:29 +0100

    Merged class Matchprotocol into Match

--- S.H.I.T._footballSolution/FootballEngine/Domain/Entities/Match.cs
+++ S.H.I.T._footballSolution/FootballEngine/Domain/Entities/Match.cs
@@ -9,20 +9,40 @@ namespace FootballEngine.Domain.Entities
 {
     class Match
     {
-        public Match(string plats, DateTime date, Guid homeTeamId, Guid visitorTeamId)
+        public Guid Id { get; set; }
+        public GeneralName Location { get; }
+        public Guid HomeTeamId { get; set; }
+        public Guid VisitorTeamId { get; set; }
+        public DateTime Date { get; set; }
+        public int[] Result { get; set; }
+        public int HomeGoals { get; set; }
+        public int VisitorGoals { get; set; }
+        public List<Guid> HomeLineup { get; set; }
+        public List<Guid> VisitorLineup { get; set; }
+        public List<Guid> RedCards { get; set; }
+        public List<Guid> YellowCards { get; set; }
+        public List<Guid> Assists { get; set; }
+        public List<Guid> Goals { get; set; }
+        public List<Guid> Injuries { get; set; }
+
+        public Match(string plats, DateTime date, Guid homeTeamId, Guid visitorTeamId, int homeGoals, int visitorGoals)
         {
             Id = new Guid();
             Date = date;
             HomeTeamId = homeTeamId;
             VisitorTeamId = visitorTeamId;
+            Result = new int[2];
+            Result[0] = homeGoals;
+            Result[1] = visitorGoals;
+            HomeLineup = new List<Guid>();
+            VisitorLineup = new List<Guid>();
+            RedCards = new List<Guid>();
+            YellowCards = new List<Guid>();
+            Assists = new List<Guid>();
+            Goals = new List<Guid>();
+            Injuries = new List<Guid>();
         }
-            
-        public Guid Id { get; set; }
-        public GeneralName Location { get; set; }
-        public Guid HomeTeamId { get; }
-        public Guid VisitorTeamId { get; }
-        public DateTime Date { get; set; }
-        // To do add Matchprotkoll
+        //To do: calculate Location
     }
            
 
