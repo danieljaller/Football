d4ed7f1ea720ad19822c2209bdb877f506c92ded
Daniel Jaller <danieljaller@gmail.com>
2016-11-02 15:46:05 +0100

    Added fake match schedule to CreateOrAdministrateSeries

---
+++ S.H.I.T._footballSolution/AdminApp/TempMatchToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace AdminApp
+{
+    class TempMatchToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var tempMatchList = (List<TempMatch>)value;
+            var schedule = new StringBuilder();
+            foreach (var tempMatch in tempMatchList)
+            {
+                schedule.AppendLine($"{tempMatch.Date}          {tempMatch.Team1}                 {tempMatch.Team2}             {tempMatch.Location}             {tempMatch.Result}");
+            }
+            return schedule;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
