f2bef5bec0b431187b7bb490b54c7380f9621a96
Sinnlos <jimmy.grunert@gmail.com>
2016-11-02 10:09:43 +0100

    Made Helper class for playerstatus.

---
+++ S.H.I.T._footballSolution/FootballEngine/Helper/PlayerStatusToString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FootballEngine.Domain.Entities.Player;
+
+namespace FootballEngine.Helper
+{
+    public static class PlayerStatusToString
+    {
+        public static string ToSwedishString(this Status status)
+        {
+            switch (status)
+            {
+                case Status.Available:
+                    return "Spelbar";
+                case Status.Suspended:
+                    return "Avstängd";
+                case Status.Injured:
+                    return "Skadad";
+                case Status.NationalTeam:
+                    return "Landslagsuppdrag";
+                case Status.Other:
+                    return "Övrig frånvaro";
+                default:
+                    return "Okänd status";
+            }
+        }
+    }
+}
