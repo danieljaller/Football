83a83044124daa7ffa467e50d0b228269ecaeda5
Jescoder91 <jeetrix1991@gmail.com>
2016-11-03 13:36:07 +0100

    Added service class SerieService.cs

---
+++ S.H.I.T._footballSolution/FootballEngine/Services/SerieService.cs
@@ -0,0 +1,41 @@
+﻿using FootballEngine.Domain.Entities;
+using FootballEngine.Interfaces;
+using FootballEngine.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballEngine.Services
+{
+    class SerieService : IService<Serie>
+    {
+        private readonly SerieRepository _serieRepository = SerieRepository.Instance;
+
+        public void Add(Serie serie)
+        {
+            _serieRepository.Add(serie);
+        }
+
+        public void Delete(Guid id)
+        {
+            _serieRepository.Delete(id);
+        }
+
+        public IEnumerable<Serie> GetAll()
+        {
+           return _serieRepository.GetAll();
+        }
+
+        public Serie GetBy(string name)
+        {
+            return _serieRepository.GetBy(name);
+        }
+
+        public Serie GetBy(Guid id)
+        {
+            return _serieRepository.GetBy(id);
+        }
+    }
+}
