6a4ea34facbd0a7a777edee08d8ac76e12f03585
nellip <co.pfeiffer@gmail.com>
2016-11-03 11:37:48 +0100

    added and changed GetBy(id) and(name) to IRepository
    added and changed GetBy(id) and(name) to PlayerRepository, MatchRepository, SerieRepository
    added FullName to Player
    created PlayerService

---
+++ S.H.I.T._footballSolution/FootballEngine/Services/PlayerService.cs
@@ -0,0 +1,40 @@
+﻿using FootballEngine.Domain.Entities;
+using FootballEngine.Interfaces;
+using FootballEngine.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballEngine.Services
+{
+    class PlayerService : IService<Player>
+    {
+        private readonly PlayerRepository _playerRepository = PlayerRepository.Instance;
+        public void Add(Player player)
+        {
+            _playerRepository.Add(player);
+        }
+
+        public void Delete(Guid id)
+        {
+            _playerRepository.Delete(id);
+        }
+
+        public IEnumerable<Player> GetAll()
+        {
+            return _playerRepository.GetAll();
+        }
+
+        public Player GetBy(string name)
+        {
+            return _playerRepository.GetById();
+        }
+
+        public Player GetBy(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
