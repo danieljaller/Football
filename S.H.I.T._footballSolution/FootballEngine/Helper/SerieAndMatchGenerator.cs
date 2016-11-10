807185f0b633f9ac19fe594d7f495d762d5d3972
Linus Gustafsson <osnusmeforfasensatan@hotmail.com>
2016-11-10 10:10:32 +0100

    Refactoring of SerieAndMatchGenerator

--- S.H.I.T._footballSolution/FootballEngine/Helper/SerieAndMatchGenerator.cs
+++ S.H.I.T._footballSolution/FootballEngine/Helper/SerieAndMatchGenerator.cs
@@ -11,69 +11,81 @@ namespace FootballEngine.Helper
     class SerieAndMatchGenerator
     {
         private MatchService matchService = new MatchService();
+        private Guid[] pairing;
+        private DateTime date;
+        private Match match;
+        private DateTime lastDate;
+        public List<Match> matches;
+        
 
-        public List<Match> SerieGenerator(List<Guid> teams)
+        public List<Match> SerieGenerator(List<Guid> teams, DateTime startDate)
         {
-            int numberOfRounds = (teams.Count - 1) * 2;
-            Guid[] pairing;
-            DateTime date;
-            DateTime lastDate = DateTime.Today;
-            var matches = new List<Match>();
+            lastDate = startDate.AddDays(-1);
+            matches = new List<Match>();
 
-            for (int i = 0; i < 2 * (teams.Count - 1); i++)
+            for (int i = 0; i < 2*(teams.Count - 1); i++)
             {
-                for (int j = 0; j < teams.Count / 2; j++)
-                {
-                    pairing = new Guid[2];
-                    if (i < teams.Count / 2)
-                    {
-                        pairing[1] = teams[j];
-                        pairing[0] = teams[teams.Count - 1 - j];
-                    }
-                    else
-                    {
-                        pairing[0] = teams[j];
-                        pairing[1] = teams[teams.Count - 1 - j];
-                    }
-
-                    if (j == teams.Count / 4 || j == 0)
-                    {
-                        lastDate = lastDate.AddDays(1);
-                    }
-
-                    if (lastDate.DayOfWeek != DayOfWeek.Saturday && lastDate.DayOfWeek != DayOfWeek.Sunday)
-                    {
-                        lastDate = lastDate.AddDays((int)DayOfWeek.Saturday - (int)DateTime.Today.DayOfWeek);
-                    }
-
-                    date = lastDate;
-
-                    Match match = new Match(date, pairing[0], pairing[1]);
-                    matchService.Add(match);
-                    matches.Add(match);
-                }
-
-
-                var tempTeams = new List<Guid>();
-                for (int j = 0; j < teams.Count; j++)
-                {
-                    if (j == 0)
-                    {
-                        tempTeams.Add(teams[0]);
-                    }
-                    else if (j == teams.Count - 1)
-                    {
-                        tempTeams.Add(teams[1]);
-                    }
-                    else
-                    {
-                        tempTeams.Add(teams[j + 1]);
-                    }
-                }
-                teams = tempTeams;
+                RoundGenerator(teams, i);
+                teams = ListShuffler(teams);
             }
             return matches;
         }
+
+        private void RoundGenerator(List<Guid> teams, int i)
+        {
+            for (int j = 0; j < teams.Count / 2; j++)
+            {
+                pairing = new Guid[2];
+                if (i < teams.Count / 2)
+                {
+                    pairing[0] = teams[j];
+                    pairing[1] = teams[teams.Count - 1 - j];
+                }
+                else
+                {
+                    pairing[1] = teams[j];
+                    pairing[0] = teams[teams.Count - 1 - j];
+                }
+
+                if (j == teams.Count / 4 || j == 0)
+                {
+                    lastDate = lastDate.AddDays(1);
+                }
+
+                if (lastDate.DayOfWeek != DayOfWeek.Saturday && lastDate.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    lastDate = lastDate.AddDays((int)DayOfWeek.Saturday - (int)lastDate.DayOfWeek);
+                }
+
+                date = lastDate;
+
+                match = new Match(date, pairing[0], pairing[1]);
+                matchService.Add(match);
+                matches.Add(match);
+            }
+        }
+
+        private List<Guid> ListShuffler(List<Guid> teams)
+        {
+            var tempTeams = new List<Guid>();
+            for (int j = 0; j < teams.Count; j++)
+            {
+                if (j == 0)
+                {
+                    tempTeams.Add(teams[0]);
+                }
+                else if (j == teams.Count - 1)
+                {
+                    tempTeams.Add(teams[1]);
+                }
+                else
+                {
+                    tempTeams.Add(teams[j + 1]);
+                }
+            }
+            return teams = tempTeams;
+        }
+
     }
 }
 
