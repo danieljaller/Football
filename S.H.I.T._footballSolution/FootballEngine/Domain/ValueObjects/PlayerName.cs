58801b55388078e85b37fa4aa2f2a464f10f5e40
Martin Lindstedt <lindstedt.martin@outlook.com>
2016-11-16 16:38:28 +0100

    Corrected bug and refactored both GeneralName and PlayerName. Added 2 tests and removed 1 test in GeneralNameTests.

--- S.H.I.T._footballSolution/FootballEngine/Domain/ValueObjects/PlayerName.cs
+++ S.H.I.T._footballSolution/FootballEngine/Domain/ValueObjects/PlayerName.cs
@@ -10,15 +10,49 @@ namespace FootballEngine.Domain.ValueObjects
 {
     public class PlayerName
     {
+        public string Value { get; set; }
+        public static int MaxLenght
+        {
+            get { return 25; }
+        }
+        public static int MinLenght
+        {
+            get { return 2; }
+        }
         public PlayerName() { }
         public PlayerName(string name)
         {
-            if (ValidName(name))
+            if (IsValidName(name))
                 Value = name;
-            else
+        }
+
+        public static bool IsValidName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new InvalidNameException("The name is either null, empty or consists only of white-space characters.");
+
+            if (name.StartsWith(" "))
+                throw new InvalidNameException("A name can not start with ' '.");
+
+            if (name.EndsWith(" "))
+                throw new InvalidNameException("A name can not end with ' '.");
+
+            if (name.Length > MaxLenght)
+                throw new InvalidNameException($"Name is too long. Maximum length is {MaxLenght} characters.");
+
+            if (name.Length < MinLenght)
+                throw new InvalidNameException($"Name is too Short. Minimum length is {MinLenght} characters.");
+
+            foreach (char character in name)
             {
-                throw new InvalidNameException("Not a valid Name");
+                if (char.IsLetter(character) || character == '-' || character == ' ')
+                    continue;
+                else
+                    throw new InvalidNameException("Name contains illegal characters. Can only contain letters, '-' and white-spaces.");
             }
+
+            return true;
+            //return Regex.IsMatch(name, @"\A[a-zA-Z´¨åäöÅÄÖ]+\s?-?[a-zA-Z´¨åäöÅÄÖ]*\z", RegexOptions.IgnoreCase);
         }
 
         public static bool TryParse(string name, out PlayerName result)
@@ -34,11 +68,6 @@ namespace FootballEngine.Domain.ValueObjects
                 return false;
             }
         }
-        public static bool ValidName(string name)
-        {
-            return Regex.IsMatch(name, @"\A[a-zA-Z´¨åäöÅÄÖ]+\s?-?[a-zA-Z´¨åäöÅÄÖ]*\z", RegexOptions.IgnoreCase);
-        }
-        public string Value { get; set; }
 
         public override string ToString()
         {
