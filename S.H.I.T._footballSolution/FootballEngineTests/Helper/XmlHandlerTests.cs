f1507c0603be07c0bfee8230d183fb2c8bf05979
Martin Lindstedt <lindstedt.martin@outlook.com>
2016-11-17 13:17:30 +0100

    Added XmlHandlerTests.cs.

---
+++ S.H.I.T._footballSolution/FootballEngineTests/Helper/XmlHandlerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FootballEngine.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FootballEngine.Domain.Entities;
+
+namespace FootballEngine.Helper.Tests
+{
+    [TestClass()]
+    public class XmlHandlerTests
+    {
+        [TestMethod()]
+        public void XmlHandler_LoadFromTest()
+        {
+            List<Player> players = XmlHandler.LoadFrom("", typeof(List<Player>)) as List<Player>;
+        }
+
+        [TestMethod()]
+        public void XmlHandler_SaveToTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void XmlHandler_SaveToTest1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
