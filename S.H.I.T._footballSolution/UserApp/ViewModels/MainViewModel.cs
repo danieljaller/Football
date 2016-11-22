f95b517a68df2a1322201b1fda6f36a84cde3296
Martin Lindstedt <lindstedt.martin@outlook.com>
2016-11-22 16:31:53 +0100

    Added MainView.xaml, MainView.xaml.cs and MainViewModel.cs.

---
+++ S.H.I.T._footballSolution/UserApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserApp.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        private string _searchTextBox;
+        public string SearchTextBox
+        {
+            get { return _searchTextBox; }
+            set { SetField(ref _searchTextBox, value); }
+        }
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}
