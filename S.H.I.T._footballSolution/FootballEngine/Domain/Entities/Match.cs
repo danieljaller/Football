cff8b6b6524181ab38f29977a31c22e37a0d92e4
Martin Lindstedt <lindstedt.martin@outlook.com>
2016-11-26 10:16:03 +0100

    Updated Match. The solution does NOT build! The errors will be fixed in the next commit.

--- S.H.I.T._footballSolution/FootballEngine/Domain/Entities/Match.cs
+++ S.H.I.T._footballSolution/FootballEngine/Domain/Entities/Match.cs
@@ -2,85 +2,76 @@
 using FootballEngine.Services;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FootballEngine.Helper;
 
 namespace FootballEngine.Domain.Entities
 {
     public class Match
     {
-        private TeamService teamService = new TeamService();
         public Guid Id { get; set; }
         public bool IsPlayed { get; set; }
-        public GeneralName Location
-        {
-            get { return teamService.GetBy(HomeTeamId).HomeArena; }
-        }
-        public Guid HomeTeamId { get; set; }
-        public Guid VisitorTeamId { get; set; }
         public MatchDate Date { get; set; }
+        public GeneralName Location { get; set; }
         public uint MatchTimeInMinutes { get; set; }
-        public List<Event> HomeGoals { get; set; }
-        public List<Event> VisitorGoals { get; set; }
-        public string Result
-        {
-            get
-            {
-                if (IsPlayed)
-                    return $"{HomeGoals.Count} - {VisitorGoals.Count}";
-                else
-                    return "-";
-            }
-        }
-        public List<Guid> HomeLineup { get; set; }
-        public List<Guid> VisitorLineup { get; set; }
-        public List<Event> HomeRedCards { get; set; }
-        public List<Event> VisitorRedCards { get; set; }
-        public List<Event> HomeYellowCards { get; set; }
-        public List<Event> VisitorYellowCards { get; set; }
+        public string Result { get { return IsPlayed ? $"{HomeGoals.Count} - {VisitorGoals.Count}" : "-"; } }
         public List<Event> HomeAssists { get; set; }
-        public List<Event> VisitorAssists { get; set; }
         public List<Exchange> HomeExchanges { get; set; }
+        public List<Event> HomeGoals { get; set; }
+        public Guid HomeTeamId { get; set; }
+        public List<Guid> HomeLineup { get; set; }
+        public List<Event> HomeRedCards { get; set; }
+        public List<Event> HomeYellowCards { get; set; }
+        public List<Event> VisitorAssists { get; set; }
         public List<Exchange> VisitorExchanges { get; set; }
-        public List<Event> Injuries { get; set; }
-        public static DateTime EndDateForMatchCreation
-        {
-            get
-            { return DateTime.Now.AddYears(5); }
-        }
+        public List<Event> VisitorGoals { get; set; }
+        public Guid VisitorTeamId { get; set; }
+        public List<Guid> VisitorLineup { get; set; }
+        public List<Event> VisitorRedCards { get; set; }
+        public List<Event> VisitorYellowCards { get; set; }
+        public static DateTime EndDateForMatchCreation { get { return DateTime.Now.AddYears(5); } }
+
         public Match() { }
 
-        public Match(MatchDate date, Guid homeTeamId, Guid visitorTeamId)
+        public Match(MatchDate date, Guid homeTeamId, Guid visitorTeamId, GeneralName location)
         {
             Id = Guid.NewGuid();
-            IsValidInparameter(date, homeTeamId, visitorTeamId);
-            Date = date;
-            MatchTimeInMinutes = 0;
-            HomeTeamId = homeTeamId;
-            VisitorTeamId = visitorTeamId;
-            HomeGoals = new List<Event>();
-            VisitorGoals = new List<Event>();
-            HomeAssists = new List<ValueObjects.Event>();
-            VisitorAssists = new List<ValueObjects.Event>();
-            HomeLineup = new List<Guid>();
-            VisitorLineup = new List<Guid>();
-            HomeRedCards = new List<Event>();
-            VisitorRedCards = new List<Event>();
-            HomeYellowCards = new List<Event>();
-            VisitorYellowCards = new List<Event>();
-            Injuries = new List<Event>();
+            if (IsValidInparameter(date, homeTeamId, visitorTeamId))
+            {
+                Date = date;
+                MatchTimeInMinutes = 0;
+
+                HomeAssists = new List<Event>();
+                HomeExchanges = new List<Exchange>();
+                HomeGoals = new List<Event>();
+                HomeTeamId = homeTeamId;
+                HomeLineup = new List<Guid>();
+                HomeRedCards = new List<Event>();
+                HomeYellowCards = new List<Event>();
+
+                VisitorAssists = new List<Event>();
+                VisitorExchanges = new List<Exchange>();
+                VisitorGoals = new List<Event>();
+                VisitorTeamId = visitorTeamId;
+                VisitorLineup = new List<Guid>();
+                VisitorRedCards = new List<Event>();
+                VisitorYellowCards = new List<Event>();
+            }
         }
+
         private bool IsValidInparameter(MatchDate date, Guid homeTeamId, Guid visitorTeamId)
         {
-            if(date.Value.Date < DateTime.Now.Date )
-            { throw new ArgumentOutOfRangeException($"Date is out of range can only be between now and two years from now."); }
+            if (date.Value.Date < DateTime.Now.Date)
+                throw new ArgumentOutOfRangeException($"Date is out of range can only be between now and {EndDateForMatchCreation} years from now.");
+
             if (date.Value.Date > EndDateForMatchCreation.Date)
-            { throw new ArgumentOutOfRangeException($"Date is out of range can only be between now and two years from now."); }
+                throw new ArgumentOutOfRangeException($"Date is out of range can only be between now and {EndDateForMatchCreation} years from now.");
+
             if (Guid.Empty == homeTeamId)
-            { throw new ArgumentException($"The homeTeramId cannot be null."); }
+                throw new ArgumentException($"The homeTeramId cannot be null.");
+
             if (Guid.Empty == visitorTeamId)
-            { throw new ArgumentException($"The visitorTeamId cannot be null."); }
+                throw new ArgumentException($"The visitorTeamId cannot be null.");
+
             return true;
         }
 
@@ -89,7 +80,7 @@ namespace FootballEngine.Domain.Entities
             return $"{HomeGoals.Count} - {VisitorGoals.Count}";
         }
     }
-           
 
-            
+
+
 }
