6eef1098c932b84618110f028128ace2a7076d65
Martin Lindstedt <lindstedt.martin@outlook.com>
2016-11-26 10:27:24 +0100

    Moved ConsoleApp.cs out of Helper directory.

--- S.H.I.T._footballSolution/TestApplication/ConsoleApp.cs
+++ S.H.I.T._footballSolution/TestApplication/ConsoleApp.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestApplication.Helper
+namespace TestApplication
 {
     // Created by Shahram Pourmakhdomi
     // Greatly improved by Martin Lindstedt
