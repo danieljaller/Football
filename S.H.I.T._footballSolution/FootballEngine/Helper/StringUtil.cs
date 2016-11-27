1aa8ba99ab201748eeffe519ad0a3b805054e886
Martin Lindstedt <lindstedt.martin@outlook.com>
2016-11-27 12:55:05 +0100

    Added CharUtil.cs, IntUtil.cs and ServiceLocator.cs. Added methods ContainsOnlyDigits() and FirstToUpper() to StringUtil.

--- S.H.I.T._footballSolution/FootballEngine/Helper/StringUtil.cs
+++ S.H.I.T._footballSolution/FootballEngine/Helper/StringUtil.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FootballEngine.Helper
+﻿namespace FootballEngine.Helper
 {
     public static class StringUtil
     {
@@ -13,6 +7,24 @@ namespace FootballEngine.Helper
             str = (ignoreCase) ? str.ToLower() : str;
             str2 = (ignoreCase) ? str2.ToLower() : str2;
             return str.Contains(str2);
+        }
+
+        public static bool ContainsOnlyDigits(this string str)
+        {
+            foreach (char c in str)
+            {
+                if (char.IsDigit(c))
+                    continue;
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public static string FirstToUpper(this string str)
+        {
+            return str[0].ToUpper() + str.Substring(1);
         }
     }
 }
