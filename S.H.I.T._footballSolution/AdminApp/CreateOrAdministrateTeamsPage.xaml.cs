9fa1512f0805279e60b76dc34d862c222215d0fb
Daniel Jaller <danieljaller@gmail.com>
2016-11-29 17:18:22 +0100

    Started to add bindings in CreateOrAdministrateTeamsPage.

--- S.H.I.T._footballSolution/AdminApp/CreateOrAdministrateTeamsPage.xaml.cs
+++ S.H.I.T._footballSolution/AdminApp/CreateOrAdministrateTeamsPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using FootballEngine.Domain.Entities;
+using FootballEngine.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,18 +25,15 @@ namespace AdminApp
         public CreateOrAdministrateTeamsPage()
         {
             InitializeComponent();
-            List<string> teamsListC = new List<string> { "Lag1", "Lag2", "Lag3", "Lag4", "Lag5", "Lag6",
-                                                         "Lag7", "Lag8", "Lag9", "Lag10", "Lag11", "Lag12",
-                                                         "Lag13", "Lag14", "Lag15", "Lag16", "Lag17", "Lag18",
-                                                         "Lag19", "Lag20"};
-            teamsList.ItemsSource = teamsListC;
+
+            teamsList.ItemsSource = ServiceLocator.Instance.TeamService.GetAll();
         }
 
         public CreateOrAdministrateTeamsPage(Team selectedTeam)
         {
             InitializeComponent();
-            List<string> teamsListC = new List<string> {selectedTeam.Name.Value};
-            teamsList.ItemsSource = teamsListC;
+            List<string> searchResultTeam = new List<string> {selectedTeam.Name.Value};
+            teamsList.ItemsSource = searchResultTeam;
         }
 
         private void NewTeamButton_Click(object sender, RoutedEventArgs e)
