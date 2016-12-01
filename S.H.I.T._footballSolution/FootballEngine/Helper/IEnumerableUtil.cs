9fa74d5f3e88f21efe51b55ff19c3fb06152ea8d
Martin Lindstedt <lindstedt.martin@outlook.com>
2016-12-01 09:40:13 +0100

    Added ToObservableCollection in IEnumerableUtil.

--- S.H.I.T._footballSolution/FootballEngine/Helper/IEnumerableUtil.cs
+++ S.H.I.T._footballSolution/FootballEngine/Helper/IEnumerableUtil.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,14 @@ namespace FootballEngine.Helper
         public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source)
         {
             return new HashSet<T>(source);
+        }
+
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> collection)
+        {
+            var c = new ObservableCollection<T>();
+            foreach (var e in collection)
+                c.Add(e);
+            return c;
         }
     }
 }
