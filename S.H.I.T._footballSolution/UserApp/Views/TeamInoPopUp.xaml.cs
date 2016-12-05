802ade069970a0b699238e424d5b9ce739a13c03
Sinnlos <jimmy.grunert@gmail.com>
2016-12-05 14:35:26 +0100

    Added SeriePage and matchPopUp

---
+++ S.H.I.T._footballSolution/UserApp/Views/TeamInoPopUp.xaml.cs
@@ -0,0 +1,34 @@
+﻿using FootballEngine.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UserApp.Views
+{
+    /// <summary>
+    /// Interaction logic for TeamInoPopUp.xaml
+    /// </summary>
+    public partial class TeamInoPopUp : Window
+    {
+        public TeamInoPopUp(Team team)
+        {
+            InitializeComponent();
+            mainFrame.Content = new TeamInfoPage();
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
